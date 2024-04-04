05a8c21aeab9b2cf6126374f22064a1f9fbf3e32
Rafael Augusto da Silva <104951242+SilvaRafael1@users.noreply.github.com>
2024-04-04 19:55:53 -0300

    Update GetRandom.jsx

--- client/src/components/GetRandom.jsx
+++ client/src/components/GetRandom.jsx
@@ -2,34 +2,41 @@ import { useEffect, useState } from "react"
 import client from "../api/Api"
 
 const GetRandom = () => {
-  // const [produto, setProduto] = useState([])
+  const [produto, setProduto] = useState([])
 
-  // const listPedido = async () => {
-  //   try {
-  //     const res = await client.get("/pedidos/random")
-  //     if (res.data) {
-  //       setProduto(res.data)
-  //     } else {
-  //       setProduto([])
-  //     }
-  //   } catch (error) {
-  //     console.error(error)
-  //   }
-  // }
+  const listPedido = async () => {
+    try {
+      const res = await client.get("/produtos/random")
+      if (res.data) {
+        setProduto(res.data)
+      } else {
+        setProduto([])
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
-  // useEffect(() => {
-  //   listPedido()
-  // }, [])
+  useEffect(() => {
+    listPedido()
+  }, [])
 
   return (
-    // <div className="flex justify-center">
-    //   {produto.map(produto => (
-    //     <div key={produto._id} className="border border-solid border-gray-600 w-[1366px] mt-2 p-5 rounded-md">
-    //       <p>{produto.name}</p>
-    //     </div>
-    //   ))}
-    // </div>
-    <div>Random Produto</div>
+    <div className="flex justify-center">
+      {produto.map(produto => (
+        <div key={produto._id} className="border border-solid border-gray-600 w-[1366px] mt-2 p-5 rounded-md flex flex-row">
+          <div className="w-[683px] border-r border-solid p-6 flex items-center justify-center">
+            <img src={produto.image} alt={produto.name} className="h-[500px]" />
+          </div>
+          <div className="w-[683px] p-6">
+            <p className="text-bold text-4xl">{produto.name}</p>
+            <p className="my-2">{produto.description}</p>
+            <p><b>Preço: </b>R${produto.price}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+    // <div>Random Produto</div>
   )
 }
 
